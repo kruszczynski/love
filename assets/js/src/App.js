5e44d32a73c180de49b1aeed6d471138c101ecc2
Bartek Kruszczynski <kruszczyk@gmail.com>
2019-09-04 00:36:46 +0200

    React seems to work

---
+++ assets/js/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Ass() {
+  return (
+    <div className="Ass">
+      <header className="Ass-header">
+        <p>
+          Edit <code>src/Ass.js</code> and save to reload.
+        </p>
+        <a
+          className="Ass-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Ass;

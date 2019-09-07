5dd1d08e85457096614a81cbaec14c7b9b2cb002
Bartek Kruszczynski <kruszczyk@gmail.com>
2019-09-07 23:48:21 +0200

    A bunch of react setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,15 +10,13 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import 'phoenix_html';
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './src/App';
-console.log(App)
+
 ReactDOM.render(<App />, document.getElementById('root'));
--- assets/js/src/App.js
+++ assets/js/src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
 
-function Ass() {
+function App() {
+  const [toast, setToast] = useState(false);
+  const handleClick = () => {
+    setToast(!toast);
+  };
   return (
-    <div className="Ass">
-      <header className="Ass-header">
-        <p>
-          Edit <code>src/Ass.js</code> and save to reload.
-        </p>
-        <a
-          className="Ass-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Jumbotron>
+            <h1>Ślub Kruszczyńskich</h1>
+            <p>
+              Będzie ślub!
+            </p>
+            <p>
+              <Button onClick={handleClick} variant="primary">IDE</Button>
+              <Button variant="primary">Bo ide</Button>
+            </p>
+            {toast && (
+            <Toast>
+              <Toast.Header>
+                <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+                <strong className="mr-auto">Bootstrap</strong>
+                <small>11 mins ago</small>
+              </Toast.Header>
+              <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+            </Toast>
+            )}
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-export default Ass;
+export default App;
